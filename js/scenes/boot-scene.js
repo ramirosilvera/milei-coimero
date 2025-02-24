007c2bb3b80c23730a96ad1669c5f2abd4cc85e8
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-02-24 14:57:09 -0300

    Update boot-scene.js

--- js/scenes/boot-scene.js
+++ js/scenes/boot-scene.js
@@ -4,8 +4,8 @@ class BootScene extends Phaser.Scene {
     }
 
     preload() {
-        // Cargar assets
-        this.load.setPath('assets/');
+        // Configurar rutas relativas correctas
+        this.load.setPath('./assets/');
         
         // Imágenes
         this.load.image('menu_bg', 'images/menu_bg.jpg');
@@ -13,10 +13,27 @@ class BootScene extends Phaser.Scene {
         this.load.image('milei', 'images/milei.png');
         this.load.image('vote_icon', 'images/vote_icon.png');
         this.load.image('boicot_icon', 'images/boicot_icon.png');
-        
+        this.load.image('debate_icon', 'images/debate_icon.png');
+        this.load.image('success_bg', 'images/success_bg.jpg');
+        this.load.image('failure_bg', 'images/failure_bg.jpg');
+
         // Sonidos
         this.load.audio('bg_music', 'sounds/background.mp3');
         this.load.audio('click_sound', 'sounds/click.wav');
+        this.load.audio('success', 'sounds/success.wav');
+        this.load.audio('failure', 'sounds/failure.wav');
+
+        // Barra de carga
+        const progressBar = this.add.graphics();
+        const progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
+        
+        this.load.on('progress', (value) => {
+            progressBar.clear();
+            progressBar.fillStyle(0x8b4513, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        });
     }
 
     create() {
