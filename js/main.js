5f768fdaf403df56bfb809a4c10056ebd268b83d
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-02-24 15:12:14 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
+// Importamos las escenas
+import BootScene from './scenes/boot-scene.js';
+import MenuScene from './scenes/menu-scene.js';
+import DecisionScene from './scenes/decision-scene.js';
+import BoicotScene from './scenes/boicot-scene.js';
+import SutilScene from './scenes/sutil-scene.js';
+import EndScene from './scenes/end-scene.js';
+
 const config = {
-    type: Phaser.WEBGL,
-    width: 1200,
-    height: 800,
-    parent: 'game-container',
-    scene: [
-        BootScene,
-        MenuScene,
-        DecisionScene,
-        BoicotScene,
-        SutilScene,
-        EndScene
-    ],
-    physics: {
-        default: 'arcade',
-        arcade: { gravity: { y: 200 } }
-    },
-    audio: {
-        disableWebAudio: false
+  type: Phaser.AUTO,
+  width: 1200,
+  height: 800,
+  scene: [BootScene, MenuScene, DecisionScene, BoicotScene, SutilScene, EndScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
     }
+  }
 };
 
 const game = new Phaser.Game(config);
-const narrativeManager = new NarrativeManager();
-const gameProgression = new GameProgression();
