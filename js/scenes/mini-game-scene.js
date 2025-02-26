aa59acdcf4bd41005de570a6f0207a4f8d11cada
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-02-26 14:46:42 -0300

    Update mini-game-scene.js

--- js/scenes/mini-game-scene.js
+++ js/scenes/mini-game-scene.js
@@ -32,7 +32,7 @@ export default class MiniGameScene extends Phaser.Scene {
     this.playerSprite = this.physics.add.sprite(0, 0, 'milei');
     this.playerSprite.setCollideWorldBounds(true);
     this.playerSprite.setBounce(0.2);
-    this.playerSprite.setScale(0.1); // Tamaño intermedio
+    this.playerSprite.setScale(0.5); // Tamaño intermedio
     this.playerContainer.add(this.playerSprite);
     
     // Controles de teclado
@@ -56,7 +56,7 @@ export default class MiniGameScene extends Phaser.Scene {
     this.enemySprite.setBounce(1);
     // Enemigo más grande y mucho más lento
     this.enemySprite.setVelocityX(-50);
-    this.enemySprite.setScale(0.2);
+    this.enemySprite.setScale(0.3);
     this.enemyContainer.add(this.enemySprite);
     
     // Crear grupo de monedas
